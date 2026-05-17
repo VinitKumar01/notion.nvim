8cb5d0abda696d2331ce30c363f05d2237347a1b
vinit <vinitk81144@gmail.com>
2026-05-17 21:42:31 +0530

    added: notion list to print selected page content

--- lua/notion/commands.lua
+++ lua/notion/commands.lua
@@ -1,9 +1,34 @@
 local M = {}
 
 function M.setup()
-	vim.api.nvim_create_user_command("Notion", function()
-		print("Notion loaded")
-	end, {})
+	local utils = require("notion.utils")
+	local api_key = vim.env.NOTION_API_KEY
+
+	vim.api.nvim_create_user_command("Notion", function(opts)
+		local subcommand = opts.fargs[1]
+
+		if subcommand == "list" then
+			utils.list(api_key)
+		elseif subcommand == "open" then
+			print("open selected")
+		elseif subcommand == "append" then
+			print("append selected")
+		else
+			vim.notify(("Unknown subcommand: %s"):format(subcommand), vim.log.levels.ERROR)
+		end
+	end, {
+		nargs = 1,
+		complete = function(ArgLead, CmdLine, CursorPos)
+			local args = vim.split(CmdLine:sub(1, CursorPos), "%s+")
+			local n = #args
+
+			if n <= 2 then
+				return { "list", "open", "append" }
+			end
+
+			return {}
+		end,
+	})
 end
 
 return M
--- lua/notion/init.lua
+++ lua/notion/init.lua
@@ -1,6 +1,17 @@
-local M = {}
+local M = {
+	config = {},
+}
 
-function M.setup()
+function M.setup(opts)
+	local api_key = vim.env.NOTION_API_KEY
+
+	M.config = vim.tbl_deep_extend("force", {}, opts or {})
+
+	if not api_key then
+		vim.notify("[notion.nvim] Missing api key in config", vim.log.levels.ERROR)
+		return
+	end
+
 	require("notion.commands").setup()
 end
 
